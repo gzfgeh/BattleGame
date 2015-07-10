403a1843b3c99c5737affbb690a8b2001e47304d
gzfgeh <gzfgeh@sina.com>
2015-07-10 21:12:11 +0800

    add png

--- Client/BattleGame/app/src/main/java/com/gzfgeh/battlegame/ui/MainDisplay.java
+++ Client/BattleGame/app/src/main/java/com/gzfgeh/battlegame/ui/MainDisplay.java
@@ -2,8 +2,10 @@ package com.gzfgeh.battlegame.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.gzfgeh.battlegame.R;
 
@@ -11,7 +13,8 @@ import com.gzfgeh.battlegame.R;
  * Created by guzhenf on 6/27/2015.
  */
 public class MainDisplay extends Activity{
-
+    private Button btnBegin;
+    private View vPlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +24,15 @@ public class MainDisplay extends Activity{
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
 
         setContentView(R.layout.main_display);
+
+        vPlay = findViewById(R.id.play);
+        btnBegin = (Button) findViewById(R.id.begin);
+        btnBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnBegin.setVisibility(View.GONE);
+                vPlay.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
