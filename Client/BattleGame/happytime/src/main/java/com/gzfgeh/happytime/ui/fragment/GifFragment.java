c4dcab2db1150dbdb52fc748b5ee137b1c1fe5b7
guzhenfu <gzfgeh@gmail.com>
2015-09-28 23:05:10 +0800

    add flowView

--- Client/BattleGame/happytime/src/main/java/com/gzfgeh/happytime/ui/fragment/GifFragment.java
+++ Client/BattleGame/happytime/src/main/java/com/gzfgeh/happytime/ui/fragment/GifFragment.java
@@ -12,6 +12,11 @@ import android.widget.ListView;
 
 import com.gzfgeh.happytime.Global;
 import com.gzfgeh.happytime.R;
+import com.gzfgeh.happytime.adapter.ImagePagerAdapter;
+import com.gzfgeh.happytime.widget.CircleFlowIndicator;
+import com.gzfgeh.happytime.widget.ViewFlow;
+
+import java.util.ArrayList;
 
 import jp.co.recruit_lifestyle.android.widget.WaveSwipeRefreshLayout;
 
@@ -21,6 +26,11 @@ import jp.co.recruit_lifestyle.android.widget.WaveSwipeRefreshLayout;
 public class GifFragment extends Fragment implements WaveSwipeRefreshLayout.OnRefreshListener {
     private ListView mListView;
     private WaveSwipeRefreshLayout mWaveSwipeRefreshLayout;
+    private ViewFlow mViewFlow;
+    private CircleFlowIndicator mFlowIndicator;
+    private ArrayList<String> imageUrlList = new ArrayList<>();
+    private ArrayList<String> linkUrlArray= new ArrayList<>();
+    private ArrayList<String> titleList= new ArrayList<>();
 
     public static GifFragment newInstance(String title){
         GifFragment fragment = new GifFragment();
@@ -38,9 +48,45 @@ public class GifFragment extends Fragment implements WaveSwipeRefreshLayout.OnRe
         mWaveSwipeRefreshLayout.setColorSchemeColors(Color.WHITE, Color.WHITE);
         mWaveSwipeRefreshLayout.setOnRefreshListener(this);
         mWaveSwipeRefreshLayout.setWaveColor(Color.RED);
-
         mListView = (ListView) rootView.findViewById(android.R.id.list);
+        initViewFlow(rootView);
         return rootView;
+    }
+
+    private void initViewFlow(View rootView) {
+        mViewFlow = (ViewFlow) rootView.findViewById(R.id.viewFlow);
+        mFlowIndicator = (CircleFlowIndicator) rootView.findViewById(R.id.viewFlowIndicator);
+        imageUrlList
+                .add("http://b.hiphotos.baidu.com/image/pic/item/d01373f082025aaf95bdf7e4f8edab64034f1a15.jpg");
+        imageUrlList
+                .add("http://g.hiphotos.baidu.com/image/pic/item/6159252dd42a2834da6660c459b5c9ea14cebf39.jpg");
+        imageUrlList
+                .add("http://d.hiphotos.baidu.com/image/pic/item/adaf2edda3cc7cd976427f6c3901213fb80e911c.jpg");
+        imageUrlList
+                .add("http://g.hiphotos.baidu.com/image/pic/item/b3119313b07eca80131de3e6932397dda1448393.jpg");
+
+        linkUrlArray
+                .add("http://blog.csdn.net/finddreams/article/details/44301359");
+        linkUrlArray
+                .add("http://blog.csdn.net/finddreams/article/details/43486527");
+        linkUrlArray
+                .add("http://blog.csdn.net/finddreams/article/details/44648121");
+        linkUrlArray
+                .add("http://blog.csdn.net/finddreams/article/details/44619589");
+        titleList.add("常见Android进阶笔试题");
+        titleList.add("GridView之仿支付宝钱包首页");
+        titleList.add("仿手机QQ网络状态条的显示与消失 ");
+        titleList.add("Android循环滚动广告条的完美实现 ");
+
+        mViewFlow.setAdapter(new ImagePagerAdapter(getActivity(), imageUrlList,
+                linkUrlArray, titleList).setInfiniteLoop(true));
+        mViewFlow.setmSideBuffer(imageUrlList.size()); // 实际图片张数，
+        // 我的ImageAdapter实际图片张数为3
+
+        mViewFlow.setFlowIndicator(mFlowIndicator);
+        mViewFlow.setTimeSpan(4500);
+        mViewFlow.setSelection(imageUrlList.size() * 1000); // 设置初始位置
+        mViewFlow.startAutoFlowTimer(); // 启动自动播放
     }
 
 
