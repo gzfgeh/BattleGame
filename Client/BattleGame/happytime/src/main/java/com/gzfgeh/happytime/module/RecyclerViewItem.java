d171d6bc6b8361422ae517356fb0d6400e00ade6
guzhenfu <gzfgeh@gmail.com>
2015-10-29 23:12:47 +0800

    add gif

---
+++ Client/BattleGame/happytime/src/main/java/com/gzfgeh/happytime/module/RecyclerViewItem.java
@@ -0,0 +1,15 @@
+package com.gzfgeh.happytime.module;
+
+import android.widget.TextView;
+
+import pl.droidsonroids.gif.GifImageView;
+
+/**
+ * Created by guzhenfu on 15/10/29.
+ */
+public class RecyclerViewItem {
+    private String tvName;
+    private GifImageView gifView;
+
+
+}
