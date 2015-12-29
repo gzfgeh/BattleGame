0f9469cb9cd3721133ae9dd0cac8fc68b43405d9
guzhenfu <gzfgeh@gmail.com>
2015-12-29 22:11:48 +0800

    add swipeback demo

--- Client/BattleGame/customview/src/main/java/com/gzfgeh/customview/CustomSwipeBack/SwipeBackActivityDemo.java
+++ Client/BattleGame/customview/src/main/java/com/gzfgeh/customview/CustomSwipeBack/SwipeBackActivityDemo.java
@@ -1,6 +1,8 @@
 package com.gzfgeh.customview.CustomSwipeBack;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.gzfgeh.customview.R;
 
@@ -8,9 +10,16 @@ import com.gzfgeh.customview.R;
  * Created by guzhenfu on 15/12/9.
  */
 public class SwipeBackActivityDemo extends SwipeBackActivity {
+    String[] s = {"1", "2", "3", "4", "5",
+                    "1", "2", "3", "4", "5",
+                        "1", "2", "3", "4", "5"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                s));
     }
 
     @Override
