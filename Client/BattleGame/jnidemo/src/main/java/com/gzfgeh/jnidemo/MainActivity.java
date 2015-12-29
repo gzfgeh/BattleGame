18cec468abbad1a229ac33e7f3fedc1ad388cc96
gzfgeh <gzfgeh@qq.com>
2015-12-29 14:34:18 +0800

    jni is ok

--- Client/BattleGame/jnidemo/src/main/java/com/gzfgeh/jnidemo/MainActivity.java
+++ Client/BattleGame/jnidemo/src/main/java/com/gzfgeh/jnidemo/MainActivity.java
@@ -8,8 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static{
+        System.loadLibrary("JniTest");
+    }
 
     public native String getStringFromNative();
 
@@ -28,27 +33,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        TextView textView = (TextView) findViewById(R.id.test);
+        textView.setText(getStringFromNative());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
